fd726d2fa50e0ceaca8114cd5ce90200877a7a23
Berkay <berkaysutlu94@gmail.com>
2023-08-09 11:54:05 +0300

    Initial Commit

---
+++ SlotGame/SlotGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotGameApp.swift
+//  SlotGame
+//
+//  Created by Berkay Sutlu on 9.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct SlotGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
